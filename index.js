4b6f75e4f8b8d8d3c24370a24b026c772cd990bd
fofiefopaelisabeth@gmail.com <fofiefopaelisabeth@gmail.com>
2024-01-13 12:27:30 +0100

    started with the styles of the muzzle app

---
+++ index.js
@@ -0,0 +1,19 @@
+// function dogbarking() {
+//     var a;
+//     a = document.getElementById("barking");
+//     a.innerHTML = "&#xf244;";
+//     setTimeout(function () {
+//       a.innerHTML = "&#xf243;";
+//     }, 1000);
+//     setTimeout(function () {
+//       a.innerHTML = "&#xf242;";
+//     }, 2000);
+//     setTimeout(function () {
+//       a.innerHTML = "&#xf241;";
+//     }, 3000);
+//     setTimeout(function () {
+//       a.innerHTML = "&#xf240;";
+//     }, 4000);
+//   }
+//   dogbarking();
+//   setInterval(dogbarking, 5000);
